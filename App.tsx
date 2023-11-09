209046f1dc037e0597f96791608363ba3c56a08e
barbara michaud <barbara.michaud@outlook.fr>
2023-11-09 13:09:40 +0100

    adding nativewind

--- App.tsx
+++ App.tsx
@@ -4,11 +4,11 @@ import { Text, View } from "react-native";
 
 const App = () => {
 	return (
-		<View>
+		<View className="flex-1 items-center justify-center">
 			<Text>HELLOOOOOOOO !</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./App.tsx", "./src/**/*.tsx"],
+	theme: {
+		colors: {},
+		extend: {},
+	},
+	plugins: [],
+};
