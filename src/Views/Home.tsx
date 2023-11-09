6636f40aed19f8ab8a6112d97f48a17d47c20d8b
LenaPancher <lenapancher@gmail.com>
2023-11-09 17:15:37 +0100

    add file constant

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,10 +1,11 @@
 import { Pressable, Text, View } from "react-native";
 import React from "react";
+import { Color } from "../base/constant";
 
 const Home = () => {
 	return (
 		<View className="flex-1 items-center justify-center">
-			<Text>je suis le home</Text>
+			<Text style={{ color: Color.PRIMARY }}>je suis le home</Text>
 			<Pressable>
 				<Text>je suis un bouton</Text>
 			</Pressable>
---
+++ src/base/constant.tsx
@@ -0,0 +1,10 @@
+export class Color {
+	static PRIMARY: string = "#58CC02";
+	static SECONDARY: string = "#48A106";
+}
+
+export class Text {}
+
+export class FontSize {}
+
+export class Url {}
