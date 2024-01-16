c52553283406fdd3f8744e0547e0af0621be719d
barbara michaud <barbara.michaud@outlook.fr>
2024-01-16 11:21:23 +0100

    adding position in home page in map

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -12,6 +12,8 @@ import { getTheCurrentPosition } from "../utils";
 import MapView from "react-native-maps";
 import { useNavigation } from "@react-navigation/core";
 import { MyNavigationProp } from "../navigation/AppNavigator";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 export enum ContentHome {
 	MAP = "Map",
@@ -20,6 +22,7 @@ export enum ContentHome {
 const Home = () => {
 	const dispatch = useAppDispatch();
 	const navigation = useNavigation<MyNavigationProp>();
+	const position = useSelector((state: RootState) => state.position.position);
 
 	useEffect(() => {
 		getTheCurrentPosition().then(location => {
@@ -44,8 +47,18 @@ const Home = () => {
 					<BoxComponent title={Content.GAME}>
 						<GameHomeComponent nextQuiz="" />
 					</BoxComponent>
-					<BoxComponent title={Content.MAP} height="h-auto" onPress={() => navigation.navigate(ContentHome.MAP)}>
-						<MapView className="w-full h-full" style={{ borderRadius: 8 }} />
+					<BoxComponent title={Content.MAP} height="h-48" onPress={() => navigation.navigate(ContentHome.MAP)}>
+						<MapView
+							className="w-full"
+							style={{ borderRadius: 8, height: "75%" }}
+							region={{
+								latitude: position?.latitude ?? 0,
+								longitude: position?.longitude ?? 0,
+								latitudeDelta: 0.0922,
+								longitudeDelta: 0.0421,
+							}}
+							showsUserLocation
+						/>
 					</BoxComponent>
 					<BoxComponent title={Content.EVENT} height="h-24">
 						<Text />
