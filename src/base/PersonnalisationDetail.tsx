d2d3363869d6769ada3751ba88f7b2aab0869cb5
LenaPancher <lenapancher@gmail.com>
2024-11-26 16:14:00 +0100

    Update CustomizationDetailComponent

--- src/base/PersonnalisationDetail.tsx
+++ src/base/PersonnalisationDetail.tsx
@@ -5,19 +5,19 @@ import { Color } from "./constant";
 import Gem from "./Gem";
 
 interface CustomizationDetailComponentProps {
-	title: string;
-	detail: string;
-	elie: ReactElement;
+	name: string;
+	description: string;
+	elie?: ReactElement;
 	gem?: number;
 }
 
-const CustomizationDetailComponent = ({ elie, title, detail, gem }: CustomizationDetailComponentProps) => {
+const CustomizationDetailComponent = ({ elie, name, description, gem }: CustomizationDetailComponentProps) => {
 	return (
-		<View className="flex-row items-center w-full justify-around mb-4">
+		<View className="flex-row items-center mb-2">
 			<View className="w-16 h-20 flex-row items-center">{elie}</View>
-			<View className="flex-col w-52">
-				<Text className="font-bold text-xl">{title}</Text>
-				<Text>{detail}</Text>
+			<View className="flex-1 flex-col mx-2">
+				<Text className="font-bold text-lg">{name}</Text>
+				<Text>{description}</Text>
 			</View>
 			{gem ? (
 				<View>
