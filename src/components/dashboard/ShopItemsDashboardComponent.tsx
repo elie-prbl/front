8d5cbffde8facd60b01f3fb72927f40f8307a745
LenaPancher <lenapancher@gmail.com>
2024-12-04 11:39:57 +0100

    List items in ShopItemsDashboardComponent

--- src/components/dashboard/ShopItemsDashboardComponent.tsx
+++ src/components/dashboard/ShopItemsDashboardComponent.tsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { getUserShopItems, UserShopItem } from "../../store/features/UserShop/UserShopService";
+import { useAppSelector } from "../../store/hooks";
+import { RootState } from "../../store/store";
+import { TypeName } from "../../store/features/Shop/ShopService";
+import BoxComponent from "../../base/Box";
+import { Color, Content } from "../../base/constant";
+import UserShopItemDetails from "./UserShopItemDetails";
 
 const ShopItemsDashboardComponent = () => {
+	const [isLoading, setLoading] = useState<boolean>(true);
+	const { user } = useAppSelector((state: RootState) => state.user);
+	const [avatarItems, setAvatarItems] = useState<UserShopItem[]>([]);
+	const [themeItems, setThemeItems] = useState<UserShopItem[]>([]);
+
+	useEffect(() => {
+		setAvatarItems([]);
+		setThemeItems([]);
+
+		(async () => {
+			try {
+				if (user) {
+					const items: UserShopItem[] = await getUserShopItems(user.uuid);
+
+					items.forEach(item => {
+						switch (item.shop_item.type.name) {
+							case TypeName.AVATAR:
+								setAvatarItems(prevState => [...prevState, item]);
+								break;
+							case TypeName.THEME:
+								setThemeItems(prevState => [...prevState, item]);
+								break;
+						}
+					});
+				}
+			} catch (e) {
+				console.log(`error ${e}`);
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, []);
+
+	if (isLoading) {
+		return <ActivityIndicator size="large" color={Color.PRIMARY} className="justify-center h-full" />;
+	}
+
 	return (
 		<View>
-			<Text>A faire dans le ticket EQ-127</Text>
+			<BoxComponent title={Content.SHOP_AVATAR}>
+				{avatarItems.map(avatar => (
+					<UserShopItemDetails key={avatar.id} userShopItem={avatar} />
+				))}
+			</BoxComponent>
+			<BoxComponent title={Content.SHOP_THEME}>
+				{themeItems.map(theme => (
+					<UserShopItemDetails key={theme.id} userShopItem={theme} />
+				))}
+			</BoxComponent>
 		</View>
 	);
 };
---
+++ src/components/dashboard/UserShopItemDetails.tsx
@@ -0,0 +1,31 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Color } from "../../base/constant";
+import { TypeName } from "../../store/features/Shop/ShopService";
+import { buildElie } from "../../utils/buildElie";
+import { UserShopItem } from "../../store/features/UserShop/UserShopService";
+
+interface UserShopItemDetailsProps {
+	userShopItem: UserShopItem;
+}
+
+const UserShopItemDetails = ({ userShopItem }: UserShopItemDetailsProps) => {
+	return (
+		<View className="flex-row items-center mb-2">
+			<View className="w-12 h-20 flex-row items-center">
+				{userShopItem.shop_item.type.name === TypeName.AVATAR && buildElie(userShopItem.shop_item.name)}
+			</View>
+			<View className="flex-1 flex-col mx-2">
+				<Text className="font-bold text-lg">{userShopItem.shop_item.name}</Text>
+				<Text>{userShopItem.shop_item.description}</Text>
+			</View>
+			{/*TODO : Ici il faudra utiliser le context pour le timing de l'application*/}
+			<TouchableOpacity onPress={() => {}} className="p-1 rounded" style={{ backgroundColor: Color.GREEN_OPACITY }}>
+				{/*TODO : Changer le text par une icône ou autre */}
+				<Text>Use</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default UserShopItemDetails;
