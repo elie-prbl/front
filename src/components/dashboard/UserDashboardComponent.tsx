19d6a1d92efc003824437264d09cca399ac067d5
LenaPancher <lenapancher@gmail.com>
2024-12-04 11:32:12 +0100

    Clean UserDashboardComponent

--- src/components/dashboard/UserDashboardComponent.tsx
+++ src/components/dashboard/UserDashboardComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 
@@ -10,8 +10,12 @@ const UserDashboardComponent = () => {
 		<View>
 			<View className="flex-col justify-center">
 				<View className="flex-row items-center">
-					<Ionicons name="mail" size={20} />
-					<Text className="ml-2">{user?.email}</Text>
+					<FontAwesome name="user" size={20} color="black" style={{ width: 20, textAlign: "center" }} />
+					<Text className="ml-2 w-5/6">{user?.username}</Text>
+				</View>
+				<View className="flex-row items-center">
+					<Ionicons name="mail" size={20} style={{ width: 20, textAlign: "center" }} />
+					<Text className="ml-2 w-5/6">{user?.email} </Text>
 				</View>
 			</View>
 		</View>
