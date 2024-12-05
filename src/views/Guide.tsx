8aa2905829b5da5e325ed4623de7e9c39cafba65
LenaPancher <lenapancher@gmail.com>
2024-12-05 11:57:02 +0100

    Add GuideTabs component

--- src/views/Guide.tsx
+++ src/views/Guide.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../base/Layout";
-import GuideFullMap from "../components/guide/GuideFullMap";
+import GuideCompactMap from "../components/guide/GuideCompactMap";
+import { Content } from "../base/constant";
+import BoxComponent from "../base/Box";
+import GuideTabs, { Tab } from "../components/guide/GuideTabs";
 
 const Guide = () => {
+	const [activeTab, setActiveTab] = useState<Tab>(Tab.EVENTS);
+
+	const handleDisplayEvents = () => {
+		setActiveTab(Tab.EVENTS);
+	};
+
+	const handleDisplayPoi = () => {
+		setActiveTab(Tab.POI);
+	};
+
 	return (
 		<Layout>
-			<GuideFullMap />
+			<BoxComponent title={Content.MAP} height="h-48" onPress={() => {}}>
+				<GuideCompactMap />
+			</BoxComponent>
+			<GuideTabs
+				activeTab={activeTab}
+				onPressEvent={() => handleDisplayEvents()}
+				onPressPoi={() => handleDisplayPoi()}
+			/>
 		</Layout>
 	);
 };
