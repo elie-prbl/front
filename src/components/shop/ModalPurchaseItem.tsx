3b541807e4807d9f8a585fdf9d80481f2a8a4b25
barbara michaud <barbara.michaud@outlook.fr>
2024-12-10 15:13:33 +0100

    fix bugs

--- src/components/shop/ModalPurchaseItem.tsx
+++ src/components/shop/ModalPurchaseItem.tsx
@@ -1,13 +1,13 @@
 import { ActivityIndicator, Alert, Modal, Text, View } from "react-native";
 import { Color, Content } from "../../base/constant";
 import { purchaseShopItem, ShopItem, TypeName } from "../../store/features/Shop/ShopService";
-import React, { useEffect, useState } from "react";
-import { ApiError } from "../../utils/ApiError";
-import { useAppSelector } from "../../store/hooks";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { RootState } from "../../store/store";
 import { buildElie } from "../../utils/buildElie";
 import ButtonComponent from "../../base/Button";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Layout from "../../base/Layout";
 
 interface ModalPurchaseItemProps {
 	activeModalPurchase: boolean;
@@ -16,83 +16,61 @@ interface ModalPurchaseItemProps {
 }
 
 const ModalPurchaseItem = ({ activeModalPurchase, shopItem, onClose }: ModalPurchaseItemProps) => {
-	const [message, setMessage] = useState("");
-	const [isLoading, setLoading] = useState(false);
 	const { user } = useAppSelector((state: RootState) => state.user);
-
-	useEffect(() => {
-		if (activeModalPurchase) {
-			setMessage("");
-		}
-	}, [activeModalPurchase]);
+	const { shop, isLoading, error } = useAppSelector((state: RootState) => state.shop);
+	const dispatch = useAppDispatch();
 
 	const handlePurchaseItem = (item: ShopItem) => {
-		(async () => {
-			try {
-				setLoading(true);
-
-				if (user) {
-					await purchaseShopItem(user.uuid, item.id);
-					Alert.alert("Votre achat a bien été effectué !");
-					onClose();
-				}
-			} catch (e: any) {
-				if (e instanceof ApiError) {
-					if (e.statusCode === 400) {
-						setMessage(e.message);
-					} else if (e.statusCode === 404) {
-						setMessage(e.message);
-					} else {
-						setMessage(e.message);
-					}
-				} else {
-					setMessage(e.message);
-				}
-			} finally {
-				setLoading(false);
-			}
-		})();
+		dispatch(purchaseShopItem({ user_uuid: user!.uuid, shop_item_id: item.id }));
+		Alert.alert("Votre achat a bien été effectué !");
+		onClose();
 	};
 
+	if (isLoading) {
+		return (
+			<Layout>
+				<ActivityIndicator size="large" color={Color.PRIMARY} className="justify-center h-full" />
+			</Layout>
+		);
+	}
+
 	return (
-		activeModalPurchase && (
-			<Modal visible={activeModalPurchase} transparent animationType="fade">
-				<View className="flex-1 items-center justify-center" style={{ backgroundColor: Color.WHITE_OPACITY }}>
-					<View
-						className="rounded-xl w-10/12 p-4 justify-evenly"
-						style={{
-							backgroundColor: Color.WHITE,
-							shadowColor: Color.GREY,
-							shadowOffset: { width: 0, height: 4 },
-							shadowOpacity: 0.9,
-							shadowRadius: 15,
-						}}>
-						<View className="items-end">
-							<Ionicons name="close-circle-outline" size={24} color={Color.BLACK} onPress={onClose} />
-						</View>
-						<View className="w-full h-36 justify-center">
-							{shopItem.type.name === TypeName.AVATAR && buildElie(shopItem.name)}
-						</View>
-						<Text className="text-lg font-bold">{shopItem.name}</Text>
-						{shopItem.type.name === TypeName.AVATAR ? (
-							<Text className="my-3">{shopItem.description}</Text>
-						) : (
-							<Text className="my-3">{shopItem.description}</Text>
-						)}
-						{message && (
-							<Text className="text-center font-bold mb-3" style={{ color: Color.RED }}>
-								{message}
-							</Text>
-						)}
-						<ButtonComponent
-							onPress={() => handlePurchaseItem(shopItem)}
-							content={isLoading ? <ActivityIndicator size="small" color={Color.WHITE} /> : Content.VALIDATE}
-							width="w-full"
-						/>
+		<Modal visible={activeModalPurchase} transparent animationType="fade">
+			<View className="flex-1 items-center justify-center" style={{ backgroundColor: Color.WHITE_OPACITY }}>
+				<View
+					className="rounded-xl w-10/12 p-4 justify-evenly"
+					style={{
+						backgroundColor: Color.WHITE,
+						shadowColor: Color.GREY,
+						shadowOffset: { width: 0, height: 4 },
+						shadowOpacity: 0.9,
+						shadowRadius: 15,
+					}}>
+					<View className="items-end">
+						<Ionicons name="close-circle-outline" size={24} color={Color.BLACK} onPress={onClose} />
 					</View>
+					<View className="w-full h-36 justify-center">
+						{shopItem.type.name === TypeName.AVATAR && buildElie(shopItem.name)}
+					</View>
+					<Text className="text-lg font-bold">{shopItem.name}</Text>
+					{shopItem.type.name === TypeName.AVATAR ? (
+						<Text className="my-3">{shopItem.description}</Text>
+					) : (
+						<Text className="my-3">{shopItem.description}</Text>
+					)}
+					{error && (
+						<Text className="text-center font-bold mb-3" style={{ color: Color.RED }}>
+							{error}
+						</Text>
+					)}
+					<ButtonComponent
+						onPress={() => handlePurchaseItem(shopItem)}
+						content={isLoading ? <ActivityIndicator size="small" color={Color.WHITE} /> : Content.VALIDATE}
+						width="w-full"
+					/>
 				</View>
-			</Modal>
-		)
+			</View>
+		</Modal>
 	);
 };
 
