edad3e8e78d110e7d0953f6bc56c5ba23edcd4ac
Paul Geneve <paulgeneve@MBP-de-Paul.lan>
2024-12-16 14:57:03 +0100

    change text component error

--- src/base/Text.tsx
+++ src/base/Text.tsx
@@ -6,15 +6,19 @@ interface TextComponentProps extends TextProps {
 	className?: string;
 	content: string | number | undefined;
 	bold?: boolean;
+	isError?: boolean;
 }
 
-const TextComponent = ({ bold, className = "", style, content, ...props }: TextComponentProps) => {
+const TextComponent = ({ bold, className = "", style, content, isError, ...props }: TextComponentProps) => {
 	const { themeVariables } = useTheme();
 
 	const finalClassName = `${className} ${bold ? "font-bold" : "font-normal"}`;
 
 	return (
-		<Text className={finalClassName} style={[{ color: themeVariables.text }, style]} {...props}>
+		<Text
+			className={finalClassName}
+			style={[{ color: isError ? themeVariables.textError : themeVariables.text }, style]}
+			{...props}>
 			{content}
 		</Text>
 	);
