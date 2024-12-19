81ab387607247ee73351f5e69cc2488385a80838
barbara michaud <barbara.michaud@outlook.fr>
2024-12-19 11:44:53 +0100

    adding disabled button

--- src/components/shop/ShopItemDetails.tsx
+++ src/components/shop/ShopItemDetails.tsx
@@ -7,12 +7,14 @@ import ModalPurchaseItem from "./ModalPurchaseItem";
 import { buildElie } from "../../utils/buildElie";
 import TextComponent from "../../base/Text";
 import { buildTheme } from "../../utils/buildTheme";
+import { UserShopItem } from "../../store/features/UserShop/UserShopService";
 
 interface ShopItemDetailsProps {
 	shopItem: ShopItem;
+	userShopItem: UserShopItem[] | null;
 }
 
-const ShopItemDetails = ({ shopItem }: ShopItemDetailsProps) => {
+const ShopItemDetails = ({ shopItem, userShopItem }: ShopItemDetailsProps) => {
 	const [activeModalPurchase, setActiveModalPurchase] = useState(false);
 
 	return (
@@ -26,11 +28,18 @@ const ShopItemDetails = ({ shopItem }: ShopItemDetailsProps) => {
 				<TextComponent content={shopItem.description} />
 			</View>
 			<TouchableOpacity
+				disabled={!!userShopItem?.find(item => item.shop_item_id === shopItem.id)}
 				onPress={() => setActiveModalPurchase(true)}
 				className="p-1 rounded"
-				style={{ backgroundColor: Color.GREEN_OPACITY }}>
+				style={{
+					backgroundColor: userShopItem?.find(item => item.shop_item_id === shopItem.id)
+						? Color.GREY_LIGHT
+						: Color.GREEN_OPACITY,
+					opacity: userShopItem?.find(item => item.shop_item_id === shopItem.id) ? 0.5 : 1,
+				}}>
 				<Gem nb={shopItem.currency} fontSize="text-lg" widthGem={22} heightGem={24} />
 			</TouchableOpacity>
+
 			<ModalPurchaseItem
 				activeModalPurchase={activeModalPurchase}
 				shopItem={shopItem}
