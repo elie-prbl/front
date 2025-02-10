247cf47f722e24da9592b5fe2c69852e316e5b74
LenaPancher <lenapancher@gmail.com>
2025-02-10 15:30:13 +0100

    Fix shop

--- src/components/shop/ModalPurchaseItem.tsx
+++ src/components/shop/ModalPurchaseItem.tsx
@@ -1,13 +1,12 @@
 import { ActivityIndicator, Alert, Modal, Text, View } from "react-native";
 import { Color, Content } from "../../base/constant";
 import { purchaseShopItem, ShopItem, TypeName } from "../../store/features/Shop/ShopService";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { RootState } from "../../store/store";
 import { buildElie } from "../../utils/buildElie";
 import ButtonComponent from "../../base/Button";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import Layout from "../../base/Layout";
 import TextComponent from "../../base/Text";
 import { useTheme } from "../../context/ThemeContext";
 import { buildTheme } from "../../utils/buildTheme";
@@ -20,23 +19,36 @@ interface ModalPurchaseItemProps {
 
 const ModalPurchaseItem = ({ activeModalPurchase, shopItem, onClose }: ModalPurchaseItemProps) => {
 	const { user } = useAppSelector((state: RootState) => state.user);
-	const { isLoading, error } = useAppSelector((state: RootState) => state.shop);
+	const { isLoading } = useAppSelector((state: RootState) => state.shop);
+	const [message, setMessage] = useState<string>("");
 	const dispatch = useAppDispatch();
 	const { themeVariables } = useTheme();
 
-	const handlePurchaseItem = (item: ShopItem) => {
-		dispatch(purchaseShopItem({ user_uuid: user!.uuid, shop_item_id: item.id }));
-		Alert.alert("Votre achat a bien été effectué !");
-		onClose();
-	};
+	useEffect(() => {
+		if (activeModalPurchase) {
+			setMessage("");
+		}
+	}, [activeModalPurchase]);
 
-	if (isLoading) {
-		return (
-			<Layout>
-				<ActivityIndicator size="large" color={Color.PRIMARY} className="justify-center h-full" />
-			</Layout>
-		);
-	}
+	const handlePurchaseItem = (item: ShopItem) => {
+		(async () => {
+			try {
+				if (user) {
+					const resultAction = await dispatch(purchaseShopItem({ user_uuid: user!.uuid, shop_item_id: item.id }));
+
+					if (purchaseShopItem.fulfilled.match(resultAction)) {
+						Alert.alert("Votre achat a bien été effectué !");
+						onClose();
+					} else if (purchaseShopItem.rejected.match(resultAction)) {
+						const error = (resultAction.payload as string) || "Une erreur est survenue";
+						setMessage(error);
+					}
+				}
+			} catch (e: any) {
+				setMessage(e.message || "Une erreur inattendue est survenue");
+			}
+		})();
+	};
 
 	return (
 		<Modal visible={activeModalPurchase} transparent animationType="fade">
@@ -63,9 +75,9 @@ const ModalPurchaseItem = ({ activeModalPurchase, shopItem, onClose }: ModalPurc
 					) : (
 						<TextComponent content={shopItem.description} className="my-3" />
 					)}
-					{error && (
+					{message && (
 						<Text className="text-center font-bold mb-3" style={{ color: Color.RED }}>
-							{error}
+							{message}
 						</Text>
 					)}
 					<ButtonComponent
